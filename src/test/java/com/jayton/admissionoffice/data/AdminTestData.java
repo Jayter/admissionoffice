f9f60d8343ab78959bd934c09a0712fa680abc45
Jayton <kompovich@gmail.com>
2016-11-26 13:00:58 +0200

    Minor fixes in AdminTestData.

--- src/test/java/com/jayton/admissionoffice/data/AdminTestData.java
+++ src/test/java/com/jayton/admissionoffice/data/AdminTestData.java
@@ -9,7 +9,7 @@ import java.time.Month;
  * Created by Jayton on 25.11.2016.
  */
 public class AdminTestData {
-    public static final Long ADMIN1_ID = 10010L;
+    public static final Long ADMIN1_ID = 10003L;
     public static final Long INCORRECT_ID = 10L;
     public static final Admin ADMIN1 = new Admin(ADMIN1_ID, "Андрій", "Пилипенко", "Київ, Васильківська 25",
             "a_pylip@gmail.com", "pylip19", "+380674562212", LocalDate.of(1984, Month.MARCH, 22));
@@ -17,7 +17,7 @@ public class AdminTestData {
             "krylov@gmail.com", "1kr45kr", "+380632245612", LocalDate.of(1995, Month.DECEMBER, 11));
     public static final Admin ADMIN3 = new Admin(ADMIN1_ID + 2, "Ігор", "Дудік", "Житомир, Польова 1",
             "duddik@gmail.com", "12dud12", "+380672235616", LocalDate.of(1989, Month.JUNE, 2));
-    public static final Admin ADMIN4 = new Admin(ADMIN1_ID + 3, "Анна", "Мохир", "Боярка, Шевченка 45",
+    public static final Admin NEW_ADMIN = new Admin(ADMIN1_ID + 3, "Анна", "Мохир", "Боярка, Шевченка 45",
             "anna.mochyr@yandex.ru", "kotik18", "+380991329782", LocalDate.of(1990, Month.JULY, 7));
 
 }
