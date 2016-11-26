b3d7b4c3ddac757cba14b498e4cac9beaf5b94f3
Jayton <kompovich@gmail.com>
2016-11-26 09:44:42 +0200

    Changed PoolHelper: removed hardcoded initialization.

--- src/main/java/com/jayton/admissionoffice/dao/jdbc/pool/PoolHelper.java
+++ src/main/java/com/jayton/admissionoffice/dao/jdbc/pool/PoolHelper.java
@@ -1,16 +1,8 @@
-package com.jayton.admissionoffice.dao.pool;
+package com.jayton.admissionoffice.dao.jdbc.pool;
 
-import com.jayton.admissionoffice.model.Subject;
 import org.apache.tomcat.jdbc.pool.DataSource;
 import org.apache.tomcat.jdbc.pool.PoolProperties;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
 import java.util.ResourceBundle;
 
 /**
@@ -18,7 +10,7 @@ import java.util.ResourceBundle;
  */
 public class PoolHelper {
 
-    private static final String DB_PROPERTIES_FILE = "db.db";
+    private static final String STANDARD_DB_PROPERTIES = "db.db";
 
     private static final String DRIVER = "db.driver";
     private static final String JDBC_URL = "db.url";
@@ -26,15 +18,17 @@ public class PoolHelper {
     private static final String DB_PASSWORD = "db.password";
 
     private static DataSource dataSource;
-    static {
-        initDataSource();
-    }
 
     private PoolHelper() {
     }
 
-    private static void initDataSource(){
-        ResourceBundle resourceBundle = ResourceBundle.getBundle(DB_PROPERTIES_FILE);
+    //should be called from listener
+    public static void initStandardDataSource() {
+        initDataSource(STANDARD_DB_PROPERTIES);
+    }
+
+    public static void initDataSource(String path) {
+        ResourceBundle resourceBundle = ResourceBundle.getBundle(path);
 
         String url = resourceBundle.getString(JDBC_URL);
         String driver = resourceBundle.getString(DRIVER);
