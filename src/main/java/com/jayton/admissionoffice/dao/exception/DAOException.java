0188f276908f87faa9ea8b943b61f7b6241b0c08
Jayton <kompovich@gmail.com>
2016-11-26 09:46:12 +0200

    Added DAOException - thrown when any exception occured during working with DB.

--- src/main/java/com/jayton/admissionoffice/dao/exception/DAOException.java
+++ src/main/java/com/jayton/admissionoffice/dao/exception/DAOException.java
@@ -4,4 +4,14 @@ package com.jayton.admissionoffice.dao.exception;
  * Created by Jayton on 25.11.2016.
  */
 public class DAOException extends Exception {
+    public DAOException() {
+    }
+
+    public DAOException(String message) {
+        super(message);
+    }
+
+    public DAOException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
