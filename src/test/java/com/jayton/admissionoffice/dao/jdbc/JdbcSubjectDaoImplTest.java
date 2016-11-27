34ef0ea7ac4686fab91d4f3ed9d28e1589d40f5b
Jayton <kompovich@gmail.com>
2016-11-27 09:04:14 +0200

    JdbcSubjectDaoImplTest extended with some additional test cases.

--- src/test/java/com/jayton/admissionoffice/dao/jdbc/JdbcSubjectDaoImplTest.java
+++ src/test/java/com/jayton/admissionoffice/dao/jdbc/JdbcSubjectDaoImplTest.java
@@ -2,6 +2,7 @@ package com.jayton.admissionoffice.dao.jdbc;
 
 import com.jayton.admissionoffice.dao.exception.DAOException;
 import com.jayton.admissionoffice.util.InitHelper;
+import com.sun.org.apache.bcel.internal.generic.NEW;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
@@ -51,6 +52,7 @@ public class JdbcSubjectDaoImplTest {
     @Test
     public void add() throws Exception {
         jdbcSubjectDao.add(NEW_SUBJECT);
+        Assert.assertEquals(SUBJECT1_ID + 3, NEW_SUBJECT.getId());
         Assert.assertEquals(NEW_SUBJECT, jdbcSubjectDao.getByName(NEW_SUBJECT.getName()));
 
         //trying to add a subject with name that already exists
