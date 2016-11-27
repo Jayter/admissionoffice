bef0a153edf0bfb5b436b6ce5d305614506052c5
Jayton <kompovich@gmail.com>
2016-11-27 09:55:23 +0200

    Created test for JdbcUniversityDaoImpl and TestData for it.

---
+++ src/test/java/com/jayton/admissionoffice/dao/jdbc/JdbcUniversityDaoImplTest.java
@@ -0,0 +1,95 @@
+package com.jayton.admissionoffice.dao.jdbc;
+
+import com.jayton.admissionoffice.dao.exception.DAOException;
+import com.jayton.admissionoffice.model.university.University;
+import com.jayton.admissionoffice.util.InitHelper;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static com.jayton.admissionoffice.data.CommonTestData.INCORRECT_ID;
+import static com.jayton.admissionoffice.data.CommonTestData.INCORRECT_NAME;
+import static com.jayton.admissionoffice.data.JdbcUniversityTestData.*;
+
+/**
+ * Created by Jayton on 26.11.2016.
+ */
+public class JdbcUniversityDaoImplTest {
+    private JdbcUniversityDaoImpl jdbcUniversityDao = JdbcUniversityDaoImpl.getInstance();
+
+    @Rule
+    public ExpectedException expected = ExpectedException.none();
+
+    @Before
+    public void setUpDb() throws Exception {
+        InitHelper.executeDbPopulate("populateByUniversities.sql");
+    }
+
+    @Test
+    public void get() throws Exception {
+        Assert.assertEquals(UNIVERSITY1, jdbcUniversityDao.get(UNIVERSITY1_ID));
+
+        Assert.assertNull(jdbcUniversityDao.get(INCORRECT_ID));
+    }
+
+    @Test
+    public void getByCity() throws Exception {
+        List<University> list = jdbcUniversityDao.getByCity(CITY);
+
+        Assert.assertEquals(list, Arrays.asList(UNIVERSITY1, UNIVERSITY2));
+
+        //if call with name that does not exist, receive an empty list, not null
+        Assert.assertEquals(jdbcUniversityDao.getByCity(INCORRECT_NAME), Collections.emptyList());
+    }
+
+    @Test
+    public void getAll() throws Exception {
+        List<University> all = jdbcUniversityDao.getAll();
+
+        Assert.assertEquals(all, Arrays.asList(UNIVERSITY1, UNIVERSITY2, UNIVERSITY3));
+    }
+
+    @Test
+    public void add() throws Exception {
+        jdbcUniversityDao.add(NEW_UNIVERSITY);
+
+        Assert.assertEquals(NEW_UNIVERSITY.getId(), UNIVERSITY1_ID + 3);
+
+        Assert.assertEquals(jdbcUniversityDao.getAll(), Arrays.asList(UNIVERSITY1, UNIVERSITY2, UNIVERSITY3, NEW_UNIVERSITY));
+
+        expected.expect(DAOException.class);
+        jdbcUniversityDao.add(UNIVERSITY_WITH_NULLABLE_FIELDS);
+    }
+
+    @Test
+    public void update() throws Exception {
+        UNIVERSITY2.setAddress("вул. Григорія Сковороди, 10");
+
+        jdbcUniversityDao.update(UNIVERSITY2);
+
+        Assert.assertEquals(jdbcUniversityDao.get(UNIVERSITY2.getId()), UNIVERSITY2);
+
+        //restore initial state
+        UNIVERSITY2.setAddress("вул. Григорія Сковороди, 2");
+
+        expected.expect(DAOException.class);
+        jdbcUniversityDao.update(NEW_UNIVERSITY);
+    }
+
+    @Test
+    public void delete() throws Exception {
+        jdbcUniversityDao.delete(UNIVERSITY3.getId());
+
+        Assert.assertEquals(jdbcUniversityDao.getAll(), Arrays.asList(UNIVERSITY1, UNIVERSITY2));
+
+        expected.expect(DAOException.class);
+        jdbcUniversityDao.delete(INCORRECT_ID);
+    }
+
+}
--- src/test/java/com/jayton/admissionoffice/data/JdbcUniversityTestData.java
+++ src/test/java/com/jayton/admissionoffice/data/JdbcUniversityTestData.java
@@ -7,6 +7,7 @@ import com.jayton.admissionoffice.model.university.University;
  */
 public class JdbcUniversityTestData {
     public static final Long UNIVERSITY1_ID = 10000L;
+    public static final String CITY = "Київ";
 
     public static final University UNIVERSITY1 = new University(UNIVERSITY1_ID,
             "Київський національний університет ім. Тараса Шевченка", "Київ", "вул. Володимирська, 60");
@@ -14,6 +15,8 @@ public class JdbcUniversityTestData {
             "Національний університет \"Києво-Могилянська Академія\"", "Київ", "вул. Григорія Сковороди, 2");
     public static final University UNIVERSITY3 = new University(UNIVERSITY1_ID + 2,
             "Національний університет \"Львівська політехніка\"", "Львів", "вул. Степана Бандери, 12");
-    public static final University UNIVERSITY4 = new University("Харківський національний університет внутрішніх справ",
+    public static final University NEW_UNIVERSITY = new University("Харківський національний університет внутрішніх справ",
             "Харків", "майдан Свободи, 4");
+    public static final University UNIVERSITY_WITH_NULLABLE_FIELDS = new University(
+            "Ще один відомо-невідомий університет", null, null);
 }
