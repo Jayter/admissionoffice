15d27534b6424fc863d275e1a590458b6d9ccf56
Jayton <kompovich@gmail.com>
2016-11-29 13:26:39 +0200

    Got rid of admin entity.

--- src/main/java/com/jayton/admissionoffice/dao/AdminDao.java
+++
@@ -1,9 +0,0 @@
-package com.jayton.admissionoffice.dao;
-
-import com.jayton.admissionoffice.model.user.Admin;
-
-/**
- * Created by Jayton on 24.11.2016.
- */
-public interface AdminDao extends BaseDao<Admin> {
-}
