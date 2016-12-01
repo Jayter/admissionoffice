54afe513d25f825d0e578950584f589c82719a0a
Jayton <kompovich@gmail.com>
2016-12-01 23:27:10 +0200

    Removed EntranceSubject class: info about entrance subjects of direction is stored in Direction class.

--- src/main/java/com/jayton/admissionoffice/model/university/Direction.java
+++ src/main/java/com/jayton/admissionoffice/model/university/Direction.java
@@ -3,24 +3,30 @@ package com.jayton.admissionoffice.model.university;
 import com.jayton.admissionoffice.model.NamedEntity;
 
 import java.math.BigDecimal;
+import java.util.Map;
 
 public class Direction extends NamedEntity {
     private BigDecimal averageCoefficient;
     private int countOfStudents;
     private Long facultyId;
+    private Map<Long, BigDecimal> entranceSubjects;
 
-    public Direction(String name, BigDecimal averageCoefficient, int countOfStudents, Long facultyId) {
+    public Direction(String name, BigDecimal averageCoefficient, int countOfStudents, Long facultyId,
+                     Map<Long, BigDecimal> entranceSubjects) {
         super(name);
         this.averageCoefficient = averageCoefficient;
         this.countOfStudents = countOfStudents;
         this.facultyId = facultyId;
+        this.entranceSubjects = entranceSubjects;
     }
 
-    public Direction(Long id, String name, BigDecimal averageCoefficient, int countOfStudents, Long facultyId) {
+    public Direction(Long id, String name, BigDecimal averageCoefficient, int countOfStudents, Long facultyId,
+                     Map<Long, BigDecimal> entranceSubjects) {
         super(id, name);
         this.averageCoefficient = averageCoefficient;
         this.countOfStudents = countOfStudents;
         this.facultyId = facultyId;
+        this.entranceSubjects = entranceSubjects;
     }
 
     public BigDecimal getAverageCoefficient() {
@@ -35,6 +41,10 @@ public class Direction extends NamedEntity {
         return facultyId;
     }
 
+    public Map<Long, BigDecimal> getEntranceSubjects() {
+        return entranceSubjects;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -44,18 +54,20 @@ public class Direction extends NamedEntity {
         Direction direction = (Direction) o;
 
         if (countOfStudents != direction.countOfStudents) return false;
-        if (averageCoefficient != null ? !(averageCoefficient.compareTo(direction.averageCoefficient) == 0) : direction.averageCoefficient != null)
+        if (averageCoefficient != null ? !averageCoefficient.equals(direction.averageCoefficient) : direction.averageCoefficient != null)
             return false;
-        return facultyId != null ? facultyId.equals(direction.facultyId) : direction.facultyId == null;
+        if (facultyId != null ? !facultyId.equals(direction.facultyId) : direction.facultyId != null) return false;
+        return entranceSubjects != null ? entranceSubjects.equals(direction.entranceSubjects) : direction.entranceSubjects == null;
 
     }
 
     @Override
     public int hashCode() {
         int result = super.hashCode();
-        result = 31 * result + (averageCoefficient != null ? averageCoefficient.hashCode() : 0);
+        result = 31 * result + (averageCoefficient != null ? averageCoefficient.intValue() : 0);
         result = 31 * result + countOfStudents;
-        result = 31 * result + (facultyId != null ? facultyId.intValue() : 0);
+        result = 31 * result + (facultyId != null ? facultyId.hashCode() : 0);
+        result = 31 * result + (entranceSubjects != null ? entranceSubjects.hashCode() : 0);
         return result;
     }
 }
