f38c4c9585164d1c09ebc1c80ee9f2d7c5ac567c
Jayton <kompovich@gmail.com>
2016-12-01 10:28:06 +0200

    Created BaseService - base interface for all services.

---
+++ src/main/java/com/jayton/admissionoffice/service/BaseService.java
@@ -0,0 +1,16 @@
+package com.jayton.admissionoffice.service;
+
+import com.jayton.admissionoffice.model.NamedEntity;
+import com.jayton.admissionoffice.service.exception.ServiceException;
+import com.sun.xml.internal.ws.wsdl.writer.document.Service;
+
+import java.util.List;
+
+/**
+ * Created by Jayton on 28.11.2016.
+ */
+public interface BaseService<T extends NamedEntity> {
+    T get(Long id) throws ServiceException;
+    boolean delete(Long id) throws ServiceException;
+    List<T> getAll() throws ServiceException;
+}
