a61e485606be1898cfa6d3528dd7041095ab4d15
Jayton <kompovich@gmail.com>
2016-12-02 10:14:03 +0200

    Added constructor to ServiceException that takes throwable as a parameter.

--- src/main/java/com/jayton/admissionoffice/service/exception/ServiceException.java
+++ src/main/java/com/jayton/admissionoffice/service/exception/ServiceException.java
@@ -7,6 +7,10 @@ public class ServiceException extends Exception {
     public ServiceException() {
     }
 
+    public ServiceException(Throwable cause) {
+        super(cause);
+    }
+
     public ServiceException(String message) {
         super(message);
     }
