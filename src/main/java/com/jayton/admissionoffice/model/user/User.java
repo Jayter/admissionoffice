4d2372b26170568838c265c04a2b68114bedad1d
Jayton <kompovich@gmail.com>
2016-12-02 10:03:28 +0200

    Changed User class: added mapof exam results.

--- src/main/java/com/jayton/admissionoffice/model/user/User.java
+++ src/main/java/com/jayton/admissionoffice/model/user/User.java
@@ -4,6 +4,7 @@ import com.jayton.admissionoffice.model.NamedEntity;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Map;
 
 public class User extends NamedEntity {
     private String lastName;
@@ -14,9 +15,10 @@ public class User extends NamedEntity {
     private String phoneNumber;
     private LocalDate birthDate;
     private BigDecimal averageMark;
+    private Map<Long, BigDecimal> results;
 
     public User(String name, String lastName, String address, String login, String email, String password,
-                String phoneNumber, LocalDate birthDate, BigDecimal averageMark) {
+                String phoneNumber, LocalDate birthDate, BigDecimal averageMark, Map<Long, BigDecimal> results) {
         super(name);
         this.lastName = lastName;
         this.address = address;
@@ -26,10 +28,11 @@ public class User extends NamedEntity {
         this.phoneNumber = phoneNumber;
         this.birthDate = birthDate;
         this.averageMark = averageMark;
+        this.results = results;
     }
 
-    public User(Long id, String name, String lastName, String address, String email,
-                String phoneNumber, LocalDate birthDate, BigDecimal averageMark) {
+    public User(Long id, String name, String lastName, String address, String email, String phoneNumber,
+                LocalDate birthDate, BigDecimal averageMark, Map<Long, BigDecimal> results) {
         super(id, name);
         this.lastName = lastName;
         this.address = address;
@@ -37,6 +40,7 @@ public class User extends NamedEntity {
         this.phoneNumber = phoneNumber;
         this.birthDate = birthDate;
         this.averageMark = averageMark;
+        this.results = results;
     }
 
     public BigDecimal getAverageMark() {
@@ -71,6 +75,10 @@ public class User extends NamedEntity {
         return birthDate;
     }
 
+    public Map<Long, BigDecimal> getResults() {
+        return results;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -81,10 +89,13 @@ public class User extends NamedEntity {
 
         if (lastName != null ? !lastName.equals(user.lastName) : user.lastName != null) return false;
         if (address != null ? !address.equals(user.address) : user.address != null) return false;
+        if (login != null ? !login.equals(user.login) : user.login != null) return false;
         if (email != null ? !email.equals(user.email) : user.email != null) return false;
+        if (password != null ? !password.equals(user.password) : user.password != null) return false;
         if (phoneNumber != null ? !phoneNumber.equals(user.phoneNumber) : user.phoneNumber != null) return false;
         if (birthDate != null ? !birthDate.equals(user.birthDate) : user.birthDate != null) return false;
-        return averageMark != null ? averageMark.compareTo(user.averageMark) == 0 : user.averageMark == null;
+        if (averageMark != null ? !(averageMark.compareTo(user.averageMark) == 0) : user.averageMark != null) return false;
+        return results != null ? results.equals(user.results) : user.results == null;
 
     }
 
@@ -93,10 +104,13 @@ public class User extends NamedEntity {
         int result = super.hashCode();
         result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
         result = 31 * result + (address != null ? address.hashCode() : 0);
+        result = 31 * result + (login != null ? login.hashCode() : 0);
         result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
         result = 31 * result + (phoneNumber != null ? phoneNumber.hashCode() : 0);
         result = 31 * result + (birthDate != null ? birthDate.hashCode() : 0);
-        result = 31 * result + (averageMark != null ? averageMark.intValue() : 0);
+        result = 31 * result + (averageMark != null ? averageMark.hashCode() : 0);
+        result = 31 * result + (results != null ? results.hashCode() : 0);
         return result;
     }
 }
