64337cd2e77a022c39351cfbe3e8c662eb074c93
Jayton <kompovich@gmail.com>
2016-12-05 11:26:22 +0200

    Extended context listener: added functionality that allows to store constant list of subjects in application context.

--- src/main/java/com/jayton/admissionoffice/controller/listener/AdmissionOfficeContextCreate.java
+++ src/main/java/com/jayton/admissionoffice/controller/listener/AdmissionOfficeContextCreate.java
@@ -1,17 +1,39 @@
 package com.jayton.admissionoffice.controller.listener;
 
 import com.jayton.admissionoffice.dao.jdbc.pool.PoolHelper;
+import com.jayton.admissionoffice.model.Subject;
+import com.jayton.admissionoffice.service.ServiceFactory;
+import com.jayton.admissionoffice.service.SubjectService;
+import com.jayton.admissionoffice.service.exception.ServiceException;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Jayton on 28.11.2016.
  */
 public class AdmissionOfficeContextCreate implements ServletContextListener {
 
-    public void contextInitialized(ServletContextEvent sce) {
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
         PoolHelper.getInstance().initStandardDataSource();
+
+        ServletContext context = servletContextEvent.getServletContext();
+        SubjectService service = ServiceFactory.getInstance().getSubjectService();
+
+        List<Subject> subjects = null;
+        try {
+            subjects = service.getAll();
+        } catch (ServiceException e) {
+            throw new RuntimeException(e);
+        }
+        Map<Long, Subject> subjectsMap = new HashMap<>();
+        subjects.forEach(subject -> subjectsMap.put(subject.getId(), subject));
+
+        context.setAttribute("subjects", subjectsMap);
     }
 
     public void contextDestroyed(ServletContextEvent sce) {
