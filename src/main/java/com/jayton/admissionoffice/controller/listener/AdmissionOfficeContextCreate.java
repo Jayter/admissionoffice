a440d43216764e9ae59b5ca55670773786b7c238
Jayton <kompovich@gmail.com>
2016-12-06 01:54:52 +0200

    Extended listener: initializes session terms.

--- src/main/java/com/jayton/admissionoffice/controller/listener/AdmissionOfficeContextCreate.java
+++ src/main/java/com/jayton/admissionoffice/controller/listener/AdmissionOfficeContextCreate.java
@@ -1,5 +1,8 @@
 package com.jayton.admissionoffice.controller.listener;
 
+import com.jayton.admissionoffice.dao.FactoryProducer;
+import com.jayton.admissionoffice.dao.UtilDao;
+import com.jayton.admissionoffice.dao.exception.DAOException;
 import com.jayton.admissionoffice.dao.jdbc.pool.PoolHelper;
 import com.jayton.admissionoffice.model.Subject;
 import com.jayton.admissionoffice.service.ServiceFactory;
@@ -9,6 +12,8 @@ import com.jayton.admissionoffice.service.exception.ServiceException;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,17 +28,22 @@ public class AdmissionOfficeContextCreate implements ServletContextListener {
 
         ServletContext context = servletContextEvent.getServletContext();
         SubjectService service = ServiceFactory.getInstance().getSubjectService();
+        UtilDao utilDao = FactoryProducer.getInstance().getPostgresDaoFactory().getUtilDao();
 
-        List<Subject> subjects = null;
+        List<Subject> subjects;
+        List<LocalDateTime> sessionTerms;
         try {
             subjects = service.getAll();
-        } catch (ServiceException e) {
+            sessionTerms = utilDao.getSessionDate(LocalDate.now().getYear());
+        } catch (ServiceException | DAOException e) {
             throw new RuntimeException(e);
         }
         Map<Long, Subject> subjectsMap = new HashMap<>();
         subjects.forEach(subject -> subjectsMap.put(subject.getId(), subject));
 
         context.setAttribute("subjects", subjectsMap);
+        context.setAttribute("sessionStart", sessionTerms.get(0));
+        context.setAttribute("sessionEnd", sessionTerms.get(1));
     }
 
     public void contextDestroyed(ServletContextEvent sce) {
