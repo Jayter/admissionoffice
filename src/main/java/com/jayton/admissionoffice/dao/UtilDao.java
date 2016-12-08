7d54ae7b1eb97bd4de70b64f1c895a4b40a61bc9
Jayton <kompovich@gmail.com>
2016-12-08 12:28:45 +0200

    Removed JdbcSubjectDao: use UtilDao instead.

--- src/main/java/com/jayton/admissionoffice/dao/UtilDao.java
+++ src/main/java/com/jayton/admissionoffice/dao/UtilDao.java
@@ -1,6 +1,7 @@
 package com.jayton.admissionoffice.dao;
 
 import com.jayton.admissionoffice.dao.exception.DAOException;
+import com.jayton.admissionoffice.model.NamedEntity;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -9,5 +10,6 @@ import java.util.List;
  * Created by Jayton on 06.12.2016.
  */
 public interface UtilDao {
+    List<NamedEntity> getAllSubjects() throws DAOException;
     List<LocalDateTime> getSessionDate(Integer currentYear) throws DAOException;
 }
--- src/main/java/com/jayton/admissionoffice/dao/jdbc/JdbcUtilDaoImpl.java
+++ src/main/java/com/jayton/admissionoffice/dao/jdbc/JdbcUtilDaoImpl.java
@@ -3,6 +3,7 @@ package com.jayton.admissionoffice.dao.jdbc;
 import com.jayton.admissionoffice.dao.UtilDao;
 import com.jayton.admissionoffice.dao.exception.DAOException;
 import com.jayton.admissionoffice.dao.jdbc.pool.PoolHelper;
+import com.jayton.admissionoffice.model.NamedEntity;
 
 import java.sql.*;
 import java.time.LocalDate;
@@ -16,6 +17,7 @@ import java.util.List;
  */
 public class JdbcUtilDaoImpl implements UtilDao {
     public static final String SQL_GET_SESSION_DATE = "SELECT * FROM session_info WHERE year=?";
+    public static final String SQL_GET_ALL = "SELECT * FROM subjects";
 
     @Override
     public List<LocalDateTime> getSessionDate(Integer currentYear) throws DAOException {
@@ -58,5 +60,46 @@ public class JdbcUtilDaoImpl implements UtilDao {
             }
         }
 
+    }
+
+    @Override
+    public List<NamedEntity> getAllSubjects() throws DAOException {
+        PreparedStatement statement = null;
+        Connection connection = null;
+
+        try {
+            connection = PoolHelper.getInstance().getDataSource().getPool().getConnection();
+            statement = connection.prepareStatement(SQL_GET_ALL);
+
+            List<NamedEntity> subjects = new ArrayList<>();
+
+            try(ResultSet rs = statement.executeQuery()) {
+                while(rs.next()) {
+                    Long id = rs.getLong("id");
+                    String name = rs.getString("name");
+
+                    subjects.add(new NamedEntity(id, name));
+                }
+            }
+            return subjects;
+
+        } catch (SQLException e) {
+            throw new DAOException("Failed to load subjects.", e);
+        } finally {
+            if(statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException e) {
+                    //will log it
+                }
+            }
+            if(connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    //will log it
+                }
+            }
+        }
     }
 }
