e0a723c8e31f474e440d0e2029b127369ea13813
Jayton <kompovich@gmail.com>
2016-12-09 12:26:13 +0200

    Changed admissionOffice context listener.

--- src/main/java/com/jayton/admissionoffice/controller/listener/AdmissionOfficeContextCreate.java
+++ src/main/java/com/jayton/admissionoffice/controller/listener/AdmissionOfficeContextCreate.java
@@ -1,52 +1,61 @@
 package com.jayton.admissionoffice.controller.listener;
 
-import com.jayton.admissionoffice.dao.FactoryProducer;
-import com.jayton.admissionoffice.dao.UtilDao;
-import com.jayton.admissionoffice.dao.exception.DAOException;
+import com.jayton.admissionoffice.dao.exception.FailedInitializationException;
 import com.jayton.admissionoffice.dao.jdbc.pool.PoolHelper;
-import com.jayton.admissionoffice.model.Subject;
+import com.jayton.admissionoffice.model.NamedEntity;
+import com.jayton.admissionoffice.model.to.SessionTerms;
 import com.jayton.admissionoffice.service.ServiceFactory;
-import com.jayton.admissionoffice.service.SubjectService;
+import com.jayton.admissionoffice.service.UtilService;
 import com.jayton.admissionoffice.service.exception.ServiceException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Created by Jayton on 28.11.2016.
- */
 public class AdmissionOfficeContextCreate implements ServletContextListener {
 
+    private final Logger logger = LoggerFactory.getLogger(ServletContextListener.class);
+
+    @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
-        PoolHelper.getInstance().initStandardDataSource();
+        try {
+            PoolHelper.getInstance().initStandardDataSource();
+        } catch (FailedInitializationException e) {
+            logger.error("Failed to initialize data source.", e);
+            throw new RuntimeException("Failed to initialize data source.");
+        }
 
         ServletContext context = servletContextEvent.getServletContext();
-        SubjectService service = ServiceFactory.getInstance().getSubjectService();
-        UtilDao utilDao = FactoryProducer.getInstance().getPostgresDaoFactory().getUtilDao();
+        UtilService utilService = ServiceFactory.getInstance().getUtilService();
 
-        List<Subject> subjects;
-        List<LocalDateTime> sessionTerms;
+        List<NamedEntity> subjects;
+        SessionTerms sessionTerms;
+
         try {
-            subjects = service.getAll();
-            sessionTerms = utilDao.getSessionDate(LocalDate.now().getYear());
-        } catch (ServiceException | DAOException e) {
-            throw new RuntimeException(e);
+            subjects = utilService.getAllSubjects();
+            sessionTerms = utilService.getSessionTerms((short) LocalDate.now().getYear());
+        } catch (ServiceException e) {
+            logger.error("Failed to load application data.", e);
+            throw new RuntimeException("Failed to load application data.", e);
         }
-        Map<Long, Subject> subjectsMap = new HashMap<>();
+
+        Map<Long, NamedEntity> subjectsMap = new HashMap<>();
         subjects.forEach(subject -> subjectsMap.put(subject.getId(), subject));
 
         context.setAttribute("subjects", subjectsMap);
-        context.setAttribute("sessionStart", sessionTerms.get(0));
-        context.setAttribute("sessionEnd", sessionTerms.get(1));
+        context.setAttribute("sessionTerms", sessionTerms);
+        logger.info("Data source is initialized.");
     }
 
+    @Override
     public void contextDestroyed(ServletContextEvent sce) {
         PoolHelper.getInstance().destroyDataSource();
+        logger.info("Data source is destroyed.");
     }
 }
