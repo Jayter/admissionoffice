bc191a92bcb75dfb522e8ebd1d962c8fcf53e7dd
Jayton <kompovich@gmail.com>
2017-01-13 21:51:59 +0200

    Extended SessionContextListener: added logging of exceptions that occurs during beans initialization.

--- src/main/java/com/jayton/admissionoffice/controller/listener/AdmissionOfficeContextCreate.java
+++ src/main/java/com/jayton/admissionoffice/controller/listener/AdmissionOfficeContextCreate.java
@@ -2,26 +2,13 @@ package com.jayton.admissionoffice.controller.listener;
 
 import com.jayton.admissionoffice.dao.exception.FailedInitializationException;
 import com.jayton.admissionoffice.dao.jdbc.pool.PoolHelper;
-import com.jayton.admissionoffice.model.Subject;
-import com.jayton.admissionoffice.model.to.SessionTerms;
-import com.jayton.admissionoffice.service.ServiceFactory;
-import com.jayton.admissionoffice.service.UtilService;
-import com.jayton.admissionoffice.service.exception.ServiceException;
 import com.jayton.admissionoffice.util.di.InjectionResolver;
+import com.jayton.admissionoffice.util.di.exception.InjectionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.xml.sax.SAXException;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class AdmissionOfficeContextCreate implements ServletContextListener {
 
@@ -33,15 +20,16 @@ public class AdmissionOfficeContextCreate implements ServletContextListener {
             PoolHelper.getInstance().initStandardDataSource();
         } catch (FailedInitializationException e) {
             logger.error("Failed to initialize data source.", e);
-            throw new RuntimeException("Failed to initialize data source.");
+            throw new RuntimeException("Failed to initialize data source.", e);
         }
 
         try {
             InjectionResolver resolver = new InjectionResolver();
             resolver.init();
             resolver.parse();
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (InjectionException e) {
+            logger.error("Failed to initialize beans.", e);
+            throw new RuntimeException("Failed to initialize beans.", e);
         }
 
         logger.info("Data source is initialized.");
