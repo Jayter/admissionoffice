42bd93c1275bc6f649b17e79d57f34e464968f5e
Jayton <kompovich@gmail.com>
2017-01-21 13:02:30 +0200

    Changed execute() method of the Command class.

--- src/main/java/com/jayton/admissionoffice/command/Command.java
+++ src/main/java/com/jayton/admissionoffice/command/Command.java
@@ -1,7 +1,9 @@
 package com.jayton.admissionoffice.command;
 
-import com.jayton.admissionoffice.util.proxy.HttpServletRequestProxy;
-
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.ResourceBundle;
 
 @FunctionalInterface
@@ -10,5 +12,16 @@ public interface Command {
     ResourceBundle PARAM_NAMES = ResourceBundle.getBundle("web.parameterAndAttributeNames");
     ResourceBundle PAGE_NAMES = ResourceBundle.getBundle("web.pageNames");
 
-    String execute(HttpServletRequestProxy request);
+    void execute(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException;
+
+    default String getRelativeReferer(HttpServletRequest request) {
+        String referer = getReferer(request);
+        String context = request.getContextPath();
+        String[] uriParts = referer.split(context);
+        return uriParts[1];
+    }
+
+    default String getReferer(HttpServletRequest request) {
+        return request.getHeader("Referer");
+    }
 }
