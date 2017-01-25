6504dac068c32d67a4d56aa4e8c3290961419d4d
Jayton <kompovich@gmail.com>
2017-01-25 15:47:27 +0200

    Changed UserFilter: removed hardcoded parameters, added initializtion from file.

--- src/main/java/com/jayton/admissionoffice/controller/filter/UserFilter.java
+++ src/main/java/com/jayton/admissionoffice/controller/filter/UserFilter.java
@@ -1,16 +1,10 @@
 package com.jayton.admissionoffice.controller.filter;
 
-import com.jayton.admissionoffice.controller.CommandHelper;
-import com.jayton.admissionoffice.controller.CommandName;
-
-import static com.jayton.admissionoffice.controller.CommandName.*;
-
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class UserFilter implements Filter {
 
@@ -18,56 +12,41 @@ public class UserFilter implements Filter {
     private static final String EXCEPTION_PAGE = "error.jsp";
     private static final String EXCEPTION = "exception";
     private static final String ACCESS_DENIED = "Access denied.";
+    private static final String ADMIN = "admin";
+    private static final String USER = "user";
 
-    private List<CommandName> adminCommands;
-    private List<CommandName> userCommands;
+    private List<String> adminCommands;
+    private List<String> userCommands;
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         adminCommands = new ArrayList<>();
         userCommands = new ArrayList<>();
 
-        adminCommands.add(CREATE_USER);
-        adminCommands.add(EDIT_USER);
-        adminCommands.add(UPDATE_USER);
-        adminCommands.add(DELETE_USER);
-        adminCommands.add(ADD_USER_RESULT);
-        adminCommands.add(DELETE_USER_RESULT);
-        adminCommands.add(CREATE_UNIVERSITY);
-        adminCommands.add(EDIT_UNIVERSITY);
-        adminCommands.add(UPDATE_UNIVERSITY);
-        adminCommands.add(DELETE_UNIVERSITY);
-        adminCommands.add(CREATE_FACULTY);
-        adminCommands.add(EDIT_FACULTY);
-        adminCommands.add(UPDATE_FACULTY);
-        adminCommands.add(DELETE_FACULTY);
-        adminCommands.add(CREATE_DIRECTION);
-        adminCommands.add(EDIT_DIRECTION);
-        adminCommands.add(UPDATE_DIRECTION);
-        adminCommands.add(DELETE_DIRECTION);
-        adminCommands.add(ADD_ENTRANCE_SUBJECT);
-        adminCommands.add(DELETE_ENTRANCE_SUBJECT);
-        adminCommands.add(ADMIN_PAGE);
-        adminCommands.add(HANDLE_APPLICATIONS);
-        adminCommands.add(EDIT_SESSION_TERMS);
-        adminCommands.add(CREATE_SESSION_TERMS);
-        adminCommands.add(UPDATE_DIRECTION);
+        String path = filterConfig.getInitParameter("commandsPath");
+        ResourceBundle commands = ResourceBundle.getBundle(path);
+        Set<String> commandKeys = commands.keySet();
 
-        userCommands.add(USER_APPLY);
-        userCommands.add(USER_CANCEL_APPLICATION);
+        for(String key: commandKeys) {
+            if(key.startsWith(ADMIN)) {
+                adminCommands.add(commands.getString(key));
+            }
+            if(key.startsWith(USER)) {
+                adminCommands.add(commands.getString(key));
+            }
+        }
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
-        String commandName = request.getParameter(COMMAND);
+        String command = request.getParameter(COMMAND);
 
-        if(commandName == null || commandName.isEmpty()) {
+        if(command == null || command.isEmpty()) {
             chain.doFilter(request, servletResponse);
             return;
         }
 
-        CommandName command = CommandHelper.getInstance().getCommandName(commandName);
         if(adminCommands.contains(command) || userCommands.contains(command)) {
             HttpSession session = request.getSession(false);
             if(session != null) {
