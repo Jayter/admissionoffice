f8290f2c32cbf361080d6af2152411e961c5067c
Jayton <kompovich@gmail.com>
2017-01-30 04:56:11 +0200

    Added cache for util dao.

--- src/main/java/com/jayton/admissionoffice/dao/jdbc/JdbcUtilDaoImpl.java
+++ src/main/java/com/jayton/admissionoffice/dao/jdbc/JdbcUtilDaoImpl.java
@@ -12,14 +12,15 @@ import org.slf4j.LoggerFactory;
 import javax.sql.DataSource;
 import java.sql.*;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class JdbcUtilDaoImpl implements UtilDao {
 
     private final ResourceBundle utilQueries = ResourceBundle.getBundle("db.queries.utilQueries");
     private final Logger logger = LoggerFactory.getLogger(JdbcUtilDaoImpl.class);
+
+    private Map<Short, SessionTerms> termsCache = new HashMap<>();
+    private List<Subject> subjectsCache = new ArrayList<>();
 
     @Injected
     private DataSource dataSource;
@@ -32,46 +33,50 @@ public class JdbcUtilDaoImpl implements UtilDao {
     @Override
     public SessionTerms getSessionTerms(short currentYear) throws DAOException {
         SessionTerms terms = null;
-        try(Connection connection = dataSource.getConnection();
-            PreparedStatement statement = connection.prepareStatement(utilQueries.getString("sessionDate.get"))) {
+        terms = termsCache.get(currentYear);
+        if(terms == null) {
+            try(Connection connection = dataSource.getConnection();
+                PreparedStatement statement = connection.prepareStatement(utilQueries.getString("sessionDate.get"))) {
 
-            statement.setShort(1, currentYear);
+                statement.setShort(1, currentYear);
 
-            try(ResultSet rs = statement.executeQuery()) {
-                if(rs.next()) {
-                    LocalDateTime start = rs.getTimestamp("session_start").toLocalDateTime();
-                    LocalDateTime end = rs.getTimestamp("session_end").toLocalDateTime();
+                try(ResultSet rs = statement.executeQuery()) {
+                    if(rs.next()) {
+                        LocalDateTime start = rs.getTimestamp("session_start").toLocalDateTime();
+                        LocalDateTime end = rs.getTimestamp("session_end").toLocalDateTime();
 
-                    terms = new SessionTerms(currentYear, start, end);
+                        terms = new SessionTerms(currentYear, start, end);
+                    }
                 }
+            } catch (SQLException e) {
+                logger.error("Failed to load session terms.", e);
+                throw new DAOException("Failed to load session terms.", e);
             }
-        } catch (SQLException e) {
-            logger.error("Failed to load session terms.", e);
-            throw new DAOException("Failed to load session terms.", e);
+            termsCache.put(currentYear, terms);
         }
         return terms;
     }
 
     @Override
     public List<Subject> getAllSubjects() throws DAOException {
-        List<Subject> subjects = new ArrayList<>();
+        if(subjectsCache.isEmpty()) {
+            try (Connection connection = dataSource.getConnection();
+                 PreparedStatement statement = connection.prepareStatement(utilQueries.getString("subject.get.all"))) {
 
-        try(Connection connection = dataSource.getConnection();
-            PreparedStatement statement = connection.prepareStatement(utilQueries.getString("subject.get.all"))) {
+                try (ResultSet rs = statement.executeQuery()) {
+                    while (rs.next()) {
+                        long id = rs.getLong("id");
+                        String name = rs.getString("name");
 
-            try(ResultSet rs = statement.executeQuery()) {
-                while(rs.next()) {
-                    long id = rs.getLong("id");
-                    String name = rs.getString("name");
-
-                    subjects.add(new Subject(id, name));
+                        subjectsCache.add(new Subject(id, name));
+                    }
                 }
+            } catch (SQLException e) {
+                logger.error("Failed to load subjects.", e);
+                throw new DAOException("Failed to load subjects.", e);
             }
-        } catch (SQLException e) {
-            logger.error("Failed to load subjects.", e);
-            throw new DAOException("Failed to load subjects.", e);
         }
-        return subjects;
+        return Collections.unmodifiableList(subjectsCache);
     }
 
     @Override
@@ -84,7 +89,13 @@ public class JdbcUtilDaoImpl implements UtilDao {
             statement.setShort(3, terms.getYear());
 
             int affectedRows = statement.executeUpdate();
-            return affectedRows != 0;
+            boolean updated = affectedRows != 0;
+
+            if(updated) {
+                termsCache.replace(terms.getYear(), terms);
+            }
+
+            return updated;
         } catch (SQLException e) {
             logger.error("Failed to update session terms.", e);
             throw new DAOException("Failed to update session terms.", e);
@@ -101,7 +112,13 @@ public class JdbcUtilDaoImpl implements UtilDao {
             statement.setTimestamp(3, Timestamp.valueOf(terms.getSessionEnd()));
 
             int affectedRows = statement.executeUpdate();
-            return affectedRows != 0;
+            boolean created = affectedRows != 0;
+
+            if (created) {
+                termsCache.put(terms.getYear(), terms);
+            }
+
+            return created;
         } catch (SQLException e) {
             logger.error("Failed to create session terms.", e);
             throw new DAOException("Failed to create session terms.", e);
